ed7bcb95b0673bb94a9258f784333d4e4950afff
Paul <dwkang@uga.edu>
2017-02-13 15:59:01 -0500

    front-page wireframe

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+class EachItem extends Component {
+    render() {
+    	return(
+    		<div className='each-item-wrapper'>
+    			<div className='col-md-4 each-item-image '><img src ='http://placehold.it/350x250'/></div>
+    			<div className='col-md-8 each-item-description '>
+    				16 Week Immersive BootcampAtlanta Tech VillageMeeting daysMon Tue Wed Thu Fri
+    				16 Week Immersive BootcampAtlanta Tech VillageMeeting daysMon Tue Wed Thu Fri
+    				16 Week Immersive BootcampAtlanta Tech VillageMeeting daysMon Tue Wed Thu Fri
+    			</div>
+    		</div>
+    	)
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='container ebay-wrapper'>
+            	<div className='sign-in-nav-bar-wrapper'>
+            		LOG IN {"/"} Register, My Ebay, Shopping Cart (each are components)
+            	</div>
+            	<div className='body-wrapper'>
+            		<div className='search-bar-wrapper'>
+            			Logo goes here
+            			<input type='text' placeholder="Search..."/>
+            			<button className='btn btn-primary'>Search</button>
+            		</div>
+            		<div className='body-content-wrapper'>
+            			<EachItem />
+            			<EachItem />
+            			<EachItem />
+            		</div>
+            	</div>
+			</div>
+        );
+    }
 }
 
 export default App;
