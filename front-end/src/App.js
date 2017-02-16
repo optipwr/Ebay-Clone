48f1a10ff739937df4c06c6748a0e10227a0c1bc
Paul <dwkang@uga.edu>
2017-02-15 23:01:09 -0500

    styled nav-bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,18 +5,21 @@ import { Link } from 'react-router';
 class App extends Component {
     render() {
         return (
-            <div className='container ebay-wrapper'>
+            <div className='ebay-wrapper'>
             	<div className='sign-in-nav-bar-wrapper'>
-                    <Link to="/login">
-                        Log in
-                    </Link>
+                    <div className='logo'><Link to='/'>Logo / Home</Link></div>
+                    <div className='search-bar-wrapper'>
+                        <Link to="/login">Sign in</Link> or
+                        <Link to="/register">Register</Link>
+                            &nbsp;&nbsp;&nbsp;
+                        <form>
+                            <input type='text' placeholder=" &nbsp;Search Items"/>
+                            <button className='btn btn-warning'>Search</button>
+                        </form>
+                    </div>
             	</div>
             	<div className='body-wrapper'>
-            		<div className='search-bar-wrapper'>
-            			Logo goes here
-            			<input type='text' placeholder="Search..."/>
-            			<button className='btn btn-primary'>Search</button>
-            		</div>
+            		
                     {this.props.children}
             	</div>
 			</div>
--- front-end/src/components/EachItemInFrontPage.js
+++ front-end/src/components/EachItemInFrontPage.js
@@ -7,18 +7,18 @@ class EachItemInFrontPage extends Component {
     }
     render() {
       var routedUrl = `/products/${this.props.item.id}`
-    	return(
-    		<div className='each-item-wrapper'>
-    			<div className='col-md-4 each-item-image '>
+      return(
+        <div className='each-item-wrapper col-sm-6'>
+          <div className='each-item-image '>
             <Link to={routedUrl}>
               <img src={this.props.item.image_url}/>
             </Link>
           </div>
-    			<div className='col-md-7 col-md-offset-1 each-item-description '>
-    				{this.props.item.name}
-    			</div>
-    		</div>
-    	)
+          <div className='each-item-description '>
+            {this.props.item.name}
+          </div>
+        </div>
+      )
     }
 }
 
